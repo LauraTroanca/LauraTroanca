b64fbd762bcd54e7374423632294a44a6b3c7324
Troanca <Troanca@DESKTOP-3FDLD4Q>
2018-09-26 16:42:37 +0300

    Merge origin/master

--- Java9LauraTroanca/src/autoservice/Autoservice.java
+++ Java9LauraTroanca/src/autoservice/Autoservice.java
@@ -20,8 +20,8 @@ public class Autoservice {
      */
     public static void main(String[] args) {
     
-        //5.a Create 3 instances of cars having different names, colors, speed, renting price and sale price
-//Create first car
+//5.a Create 3 instances of cars having different names, colors, speed, renting price and sale price
+    //Create first car
     m1 = new Car("Mercedes", (short)130, YELLOW);
     m1.setRentPrice(100);
     m1.setSalePrice(15000);
@@ -31,7 +31,7 @@ public class Autoservice {
     m2.setRentPrice(80);
     m2.setSalePrice(10000);
     
-        //Create third car
+    //Create third car
     m3 = new Car("Renault", (short)85, WHITE);
     m3.setRentPrice(90);
     m3.setSalePrice(11000);
@@ -52,20 +52,20 @@ public class Autoservice {
  * Method display all info in autoservice
  */
     
-    
-
-
 //5.c Display all informations about all cars calling the method displayAutoservice()
     displayAutoservice();
+    
 //5.d Increase speed of first car with 10
-    m1.increaseSpeed((short)(m1.getSpeed())+10);
+    m1.increaseSpeed(((short)m1.getSpeed()+10));
 
 //5.e Decrease speed of second car with 10
-
+    m2.decreaseSpeed(((short)(m2.getSpeed())-10));
+    
 //5.f Increase renting price of third car with 15 and decrease its sale price with 1200
-
+    m3.decreaseSpeed(((short)(m2.getSpeed())-10));
+    
 //5.g Display all informations about all cars calling the method displayAutoservice()
-
+    displayAutoservice()
     }
     
         private static void displayAutoservice() {
@@ -75,7 +75,18 @@ public class Autoservice {
        System.out.println("Car speed:" + m1.getSpeed());
        System.out.println("Car renting price:" + m1.getRentPrice());
        System.out.println("Car sale price" + m1.getSalePrice());
-    
+       
+       System.out.println("Car name:" + m2.getName());
+       System.out.println("Car color:" + m2.getColor());
+       System.out.println("Car speed:" + m2.getSpeed());
+       System.out.println("Car renting price:" + m2.getRentPrice());
+       System.out.println("Car sale price" + m2.getSalePrice());
+       
+       System.out.println("Car name:" + m3.getName());
+       System.out.println("Car color:" + m3.getColor());
+       System.out.println("Car speed:" + m3.getSpeed());
+       System.out.println("Car renting price:" + m3.getRentPrice());
+       System.out.println("Car sale price" + m3.getSalePrice());
     } 
  
  }
