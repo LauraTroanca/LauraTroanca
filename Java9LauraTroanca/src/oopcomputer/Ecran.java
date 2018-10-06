91fac32a0b47f4fdafe968024b0727a2f46567a8
Troanca <Troanca@DESKTOP-3FDLD4Q>
2018-10-06 20:08:54 +0300

    Merge origin/master

--- Java9LauraTroanca/src/oopcomputer/Ecran.java
+++ Java9LauraTroanca/src/oopcomputer/Ecran.java
@@ -10,14 +10,45 @@ package oopcomputer;
 public class Ecran {
     
     private String afisaj = "0";
+    private Carcasa stapana;
+
+    public Ecran(Carcasa NouaStapana) {
+        this.stapana = NouaStapana;
+    }
 
     public String getAfisaj() {
         return afisaj;
     }
+    
+    
 
     public void setAfisaj(String afisaj) {
-        this.afisaj = afisaj;
+        switch (afisaj) {
+            case "+":
+                
+                afisaj="+";
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            case "=":
+                break;
+            default: 
+            
+                
+        }
+        
+        
+        
     }
     
+    
+    private void operatiuneLocala(char c){
+    stapana.getAlu().setOperandUnu(Integer.getInteger(afisaj));
+                stapana.getAlu().setOperator('+');
+}
     
 }
